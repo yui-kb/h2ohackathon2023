509f4ad0502012735061199a5ef7719ed7437d13
technologeve <eve.nicole@icloud.com>
2023-01-21 16:45:52 +0000

    Add comments

--- read_data_to_classes.py
+++ read_data_to_classes.py
@@ -1,6 +1,8 @@
 
+# External imports
 import pandas as pd
 
+# Internal imports
 from Hackathon2023Classes import Monitor, pipe, material
 
 def set_up_system():
@@ -9,12 +11,10 @@ def set_up_system():
     # Load material data
     material_dict = {}
     materialsdf = pd.read_csv("data/materialData.csv", index_col="Material")
-    # materials_list = [f'{i}_{material(materialsdf.loc[i]["ChemicalErosionFactor"], materialsdf.loc[i]["WaterErosionFactor"], materialsdf.loc[i]["Strength"])}' for i in materialsdf.index]
+
     for i in materialsdf.index:
         material_dict[i] = material(materialsdf.loc[i]["ChemicalErosionFactor"], materialsdf.loc[i]["WaterErosionFactor"], materialsdf.loc[i]["Strength"])
-    # lead = material(materialsdf.loc["Lead"]["ChemicalErosionFactor"], materialsdf.loc["Lead"]["WaterErosionFactor"], materialsdf.loc["Lead"]["Strength"])
-    # print(/materials_list)
-    # print(lead)
+
 
 
     # Set up all our monitors
