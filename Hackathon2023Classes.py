8ea07c83b1eec5e9d00de298d913826027a7e1e8
technologeve <eve.nicole@icloud.com>
2023-01-21 16:32:14 +0000

    Working with data

--- Hackathon2023Classes.py
+++ Hackathon2023Classes.py
@@ -1,4 +1,6 @@
-import random 
+import random
+
+from temp_change import temp_change
 from datetime import date, strptime
 class Monitor:
     def __init__(self,longitude,latitude,curFlow,avgFlow):
@@ -8,8 +10,8 @@ class Monitor:
         self.avgFlow = float(avgFlow)
     def update(self):
         self.pressure = random.uniform(0,20.5)
-  
-    
+
+
 class pipe:
     def __init__(self,start,end,depth,sewage,material,instalDate):
         self.start = start
@@ -23,7 +25,7 @@ class pipe:
     def returnPipeAge(self):
         intstalDateTime = strptime(self.instalDate, "%d%m%y")
         today = date.today()
-        return today.year - instalDateTime.year - ((today.month,today.day)<(intstalDateTime.month,intstalDateTime.day))
+        return today.year - intstalDateTime.year - ((today.month,today.day)<(intstalDateTime.month,intstalDateTime.day))
 
 
 
@@ -33,21 +35,27 @@ class pipe:
             return True
         else:
             return False
+
     def ErRisk(self):
         if self.sewage:
             erRate = self.material.sewageRate
         else:
             erRate = self.material.cleanRate
         erRisk = self.returnPipeAge() * erRate
-        return erRate
+        return erRisk
+
     def TempRisk(self):
-        tempChanges = (tempChange(self.start.longitude,self.start.latitude)+tempChange(end.start.longitude,end.start.latitude)) / 2
-        tempRisk = tempChanges / depth
-    
+        chance_of_thaw = temp_change()
+        tempRisk = chance_of_thaw / self.depth
+        return tempRisk
+
     def risk(self):
         riskPoint = self.TempRisk() + self.ErRisk()
+        return riskPoint
 
+class material():
+    def __init__(self, sewageRate, cleanRate, Strength):
+        self.sewageRate = sewageRate
+        self.cleanRate = cleanRate
+        self.Strength = Strength
 
-
-def tempChange(long,lat):
-    #work out temp changes at given loc
--- read_data_to_classes.py
+++ read_data_to_classes.py
@@ -1,16 +1,38 @@
 
 import pandas as pd
 
-from Hackathon2023Classes import Monitor
-
+from Hackathon2023Classes import Monitor, pipe, material
 
 def set_up_system():
+    """ Read in our theoretical datasets and put them in their respective classes """
 
+    # Load material data
+    material_dict = {}
+    materialsdf = pd.read_csv("data/materialData.csv", index_col="Material")
+    # materials_list = [f'{i}_{material(materialsdf.loc[i]["ChemicalErosionFactor"], materialsdf.loc[i]["WaterErosionFactor"], materialsdf.loc[i]["Strength"])}' for i in materialsdf.index]
+    for i in materialsdf.index:
+        material_dict[i] = material(materialsdf.loc[i]["ChemicalErosionFactor"], materialsdf.loc[i]["WaterErosionFactor"], materialsdf.loc[i]["Strength"])
+    # lead = material(materialsdf.loc["Lead"]["ChemicalErosionFactor"], materialsdf.loc["Lead"]["WaterErosionFactor"], materialsdf.loc["Lead"]["Strength"])
+    # print(/materials_list)
+    # print(lead)
+
+
+    # Set up all our monitors
     df = pd.read_csv("data/monitorData.csv")
-
     monitors = [None] * len(df.index)
 
     for ind in df.index:
         monitors[ind] = Monitor(df["longitude"][ind],df["latitude"][ind],df["curFlow"][ind],df["avgFlow"][ind])
 
-set_up_system()
+
+    # Set up all our pipes
+    pipesdf = pd.read_csv("data/pipedata.csv")
+
+    pipes = [None] * len(pipesdf.index)
+    for ind in pipesdf.index:
+        pipes[ind] = pipe(monitors[pipesdf["pipe_start"][ind]-1],
+            monitors[pipesdf["pipe_end"][ind]-1],pipesdf["depth"][ind],
+            pipesdf["fluid"][ind], material_dict[pipesdf["material"][ind]],
+            pipesdf["date_installed"][ind])
+
+    return material_dict, monitors, pipes
